c385ef5056c8b63134c7a6a127591b551376a8c2
Le Hoang Phuc <phucyugi1@gmail.com>
2020-12-04 12:36:41 +0700

    add menu category container

--- ServerNodejs/public/javascript/Star.js
+++ ServerNodejs/public/javascript/Star.js
@@ -2,6 +2,5 @@
 const starsTotal=5;
 function getRatings(numberRating){
     const starPercentage=numberRating/starsTotal *100;
-    console.log(starPercentage);
     document.querySelector(`.stars-inner`).style.width=`${starPercentage}%`;
 }
---
+++ ServerNodejs/public/javascript/menu-hover.js
@@ -0,0 +1,6 @@
+var element=document.getElementById("category-1");
+console.log(element);
+
+/*element.addEventListener("mouseenter", function(event){
+    console.log("Mouse in");
+  });*/
