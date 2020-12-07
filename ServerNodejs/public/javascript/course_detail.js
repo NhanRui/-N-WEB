77cd6d2e18a4d1b013645fc857e5b1b375e33d83
18127090-JustDuy <51396385+18127090-JustDuy@users.noreply.github.com>
2020-12-07 12:47:52 +0700

    Update course_deital.css

--- ServerNodejs/public/javascript/course_detail.js
+++ ServerNodejs/public/javascript/course_detail.js
@@ -1,6 +1,19 @@
-window.addEventListener('scroll', (e) => {
-    if (document.body.scroll < 10)
-        document.getElementById('intro-img').style.display = 'none';
-    else
-        document.getElementById('intro-img').style.display = 'block';
-});
+const categories = document.querySelector(".categories");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      let intro_Image = document.querySelector(".intro-img");
+      if (entry.isIntersecting === false) {
+        intro_Image.classList.add('m-fadeOut');
+        intro_Image.classList.remove('m-fadeIn');
+      } else {
+        intro_Image.classList.add('m-fadeIn');
+        intro_Image.classList.remove('m-fadeOut');
+      }
+    });
+  },
+  { threshold: [0] }
+);
+
+observer.observe(categories);
