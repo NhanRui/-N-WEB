80f8b9cbc24547828eaf3ee98593380c189ed375
Nhân Rùi <nhanluu838@gmail.com>
2021-01-02 12:23:51 +0700

    sua db len khung upload video

--- ServerNodejs/models/lecturer_model.js
+++ ServerNodejs/models/lecturer_model.js
@@ -27,5 +27,29 @@ module.exports ={
         if(rows.length === 0)
             return null;
         return rows;
+    },
+
+    async getLessonList(course_id){
+        const sql = `select * from lesson_list where course_id = '${course_id}' order by chapter_number`;
+        const [rows, fields] = await db.load(sql);
+        if(rows.length === 0)
+            return null;
+        return rows;
+    },
+
+    async getVideoList(lesson_id){
+        const sql = `select * from video where list_id = '${lesson_id}'`;
+        const [rows, fields] = await db.load(sql);
+        if(rows.length === 0)
+            return null;
+        return rows;
+    },
+
+    async getCourse(course_id){
+        const sql = `select * from course where course_id = '${course_id}'`;
+        const [rows, fields] = await db.load(sql);
+        if(rows.length === 0)
+            return null;
+        return rows;
     }
 }
